03457fc09d03cff932da364e6374c8c810610728
소정 <ysj1751@naver.com>
2023-05-26 19:38:48 +0900

    로그인 함수 완료

--- src/main/java/project/diary/DiaryApplication.java
+++ src/main/java/project/diary/DiaryApplication.java
@@ -2,13 +2,25 @@ package project.diary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DiaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiaryApplication.class, args);
+	}
+	@Controller
+	public class HomeController {
+
+		@GetMapping("/")
+		public String login(Model model) {
+			// 모델에 필요한 데이터를 추가할 수도 있습니다.
+			model.addAttribute("message", "Welcome to the Diary Application!");
+			return "Login.html";
+		}
 	}
 
 }
