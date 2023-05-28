c768b174a207e181aa3a20b70fb1bb5c2120f98b
소정 <ysj1751@naver.com>
2023-05-28 22:06:23 +0900

    헤더 토큰 인증 다이어리 조회, GET /calendar 추가

--- src/main/java/project/diary/DiaryApplication.java
+++ src/main/java/project/diary/DiaryApplication.java
@@ -22,14 +22,36 @@ public class DiaryApplication {
 			model.addAttribute("message", "Welcome to the Diary Application!");
 			return "Login.html";
 		}
+	}
+	@Controller
+	public static class SignupController {
 
-		@Controller
-		public static class SignupController {
-
-			@GetMapping("/api/user/signup")
-			public String signup() {
-				return "Signup.html";
-			}
+		@GetMapping("/api/user/signup")
+		public String signup() {
+			return "Signup.html";
 		}
 	}
+
+	@Controller
+	public static class CalendarController {
+
+		@GetMapping("/api/user/calendar")
+		public String calendar() {
+			return "Calendar.html";
+		}
+	}
+
+
+	@Controller
+	public static class MyPageController {
+
+		@GetMapping("/api/user/mypage/{userLoginId}/my")
+		public String myPage() {
+			return "MyPage.html";
+		}
+	}
+
+
+
+
 }
